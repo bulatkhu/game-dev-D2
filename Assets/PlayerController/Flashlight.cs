4212a6023d70cc566e1bc4f869bd9b084d119d04
bulatkhu <hudainazarov.b@gmail.com>
2023-07-09 18:50:07 +0200

    added sound for flashlight

--- Assets/PlayerController/Flashlight.cs
+++ Assets/PlayerController/Flashlight.cs
@@ -1,33 +1,38 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Flashlight : MonoBehaviour
 {
     [SerializeField] private GameObject Light;
+    [SerializeField] private AudioClip flashlightSound;
+
     private bool IsOn = false;
-    
+    private AudioSource audioSource;
+
     // Start is called before the first frame update
     void Start()
     {
         Light.gameObject.SetActive(false);
+        audioSource = GetComponent<AudioSource>();
+        // Assign the flashlightSound clip to the AudioSource
     }
 
     // Update is called once per frame
     void Update()
     {
+        audioSource.clip = flashlightSound;
         if (Input.GetKeyDown(KeyCode.F))
         {
             if (IsOn == false)
             {
                 Light.gameObject.SetActive(true);
                 IsOn = true;
+                audioSource.Play();
             }
             else 
             {
-                
                 Light.gameObject.SetActive(false);
                 IsOn = false;
+                audioSource.Play();
             }
         }
     }
