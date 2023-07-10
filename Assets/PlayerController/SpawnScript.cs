8f09e8599be44bf2cec70fcc0e3f4dfe04cd64b4
bulatkhu <hudainazarov.b@gmail.com>
2023-07-10 10:02:56 +0200

    spawning cubes and changing the materials

--- Assets/PlayerController/SpawnScript.cs
+++ Assets/PlayerController/SpawnScript.cs
@@ -1,22 +1,50 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class SpawnScript : MonoBehaviour
 {
-    public GameObject cubePrefab;
+    public GameObject[] cubePrefabs;
+    
+    [SerializeField]
+    public int currentCubeIndex = 0;
     
     // Update is called once per frame
     void Update()
     {
+        handleCurrentIndex();
+        
         if (Input.GetKeyDown(KeyCode.Mouse0))
         {
-            Instantiate(cubePrefab, transform.position, Quaternion.identity);
+            Instantiate(cubePrefabs[currentCubeIndex], transform.position, Quaternion.identity);
         }
 
         if (Input.GetKeyDown(KeyCode.Mouse1))
         {
             Debug.Log("Right mouse click");
+        }
+    }
+
+    void handleCurrentIndex()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            currentCubeIndex = 0;
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            currentCubeIndex = 1;
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            currentCubeIndex = 2;
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha4))
+        {
+            currentCubeIndex = 3;
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha5))
+        {
+            currentCubeIndex = 4;
         }
     }
 }
