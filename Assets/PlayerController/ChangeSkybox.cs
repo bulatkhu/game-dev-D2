d33efe9cacce58bee39eb559151c6b54a1afa524
bulatkhu <hudainazarov.b@gmail.com>
2023-07-11 13:52:15 +0200

    increasing/decreasing light intensity depending on the time

--- Assets/PlayerController/ChangeSkybox.cs
+++ Assets/PlayerController/ChangeSkybox.cs
@@ -2,20 +2,70 @@ using UnityEngine;
 
 public class ChangeSkybox : MonoBehaviour
 {
+    public Light targetLight;
+    public float nightRotationSpeed = 0.5f;
     public Material daySkybox;
     public Material nightSkybox;
 
-    // Update is called once per frame
+    private bool isDaytime = true;
+    
+    void Start()
+    {
+        // Ensure you have a reference to the target light component
+        if (targetLight == null)
+        {
+            targetLight = GetComponent<Light>();
+        }
+    }
+
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Q))
         {
-            RenderSettings.skybox = nightSkybox;
+            SetNighttime();
         }
 
         if (Input.GetKeyDown(KeyCode.E))
         {
-            RenderSettings.skybox = daySkybox;
+            SetDaytime();
         }
+   
+        // float rotationSpeed = isDaytime ? dayRotationSpeed : nightRotationSpeed;
+        // sunTransform.Rotate(Vector3.right * (rotationSpeed * Time.deltaTime));
+        // // Check sun's rotation angle to switch skybox
+        // if (sunTransform.rotation.eulerAngles.x >= 180 && isDaytime)
+        // {
+        //     SetNighttime();
+        // }
+        // else if (sunTransform.rotation.eulerAngles.x < 180 && !isDaytime)
+        // {
+        //     SetDaytime();
+        // }
+    }
+   
+    void SetDaytime()
+    {
+        if (isDaytime)
+        {
+            return;
+        }
+        
+        isDaytime = true;
+        RenderSettings.skybox = daySkybox;
+        transform.Rotate(Vector3.right * 180);
+        targetLight.intensity = 2;
+    }
+   
+    void SetNighttime()
+    {
+        if (!isDaytime)
+        {
+            return;
+        }
+        
+        isDaytime = false;
+        RenderSettings.skybox = nightSkybox;
+        transform.Rotate(Vector3.left * 180);
+        targetLight.intensity = 0;
     }
 }
